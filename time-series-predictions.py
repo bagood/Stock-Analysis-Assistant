638d77b78ba96a094b510d9ca3a6e74c43134f59
bagood <4riabagus@gmail.com>
2022-12-25 13:53:19 +0700

    added some documentations for the functions

--- time-series-predictions.py
+++ time-series-predictions.py
@@ -14,10 +14,24 @@ class TimeSeriesPredictions:
         simplefilter("ignore")
     
     def scrape_stock_price(self, start, end, emiten):
+        """Collects an emiten's stocks price data from yahoo finance
+        :param start: a datetime object that tells the first date where the datas are being collected
+        :param end: a datetime object that tells the last date where the datas are being collected
+        :param emiten: a string that represents the emiten's code that is listed in the stock market
+        :return stock_data: a pandas dataframe that contain the emiten's stocks data
+        """
         stock_data = yf.download(emiten.upper() + '.JK', start, end).to_period('D')
         return stock_data
     
     def time_series_analysis_model(self, fourier_pairs, stock_data):
+        """Creates the time series analysis model that fits the stock's price over a set period of time
+        :param fourier_pairs: an integer that represents the number of fourier pairs used to approximate the stock price
+        :param stock_data: a pandas dataframe that contain the emiten's stocks data
+        :return target_pred: a numpy arrays consisting of predictions made for the stock's price
+        :return target_fore: a numpy arrays that consist 1 day forecast of the stock's price
+        :return perc: a float that represents the stock's price percentage increase on the next day 
+        :return rmse: a float that represents the root mean squared of the predictions made
+        """
         target = stock_data['Close']
         fourier = CalendarFourier(freq="A", order=fourier_pairs) 
         dp = DeterministicProcess(
@@ -40,6 +54,13 @@ class TimeSeriesPredictions:
         return (target_pred, target_fore, perc, rmse)        
 
     def auto_rejection_stock_boundaries(self, stock_data, target_fore, perc):
+        """Tunes the stock's price increase when it is outside the auto rejection boundaries
+        :param stock_data: a pandas dataframe that contain the emiten's stocks data
+        :param target_fore: a numpy arrays that consist 1 day forecast of the stock's price
+        :param perc: a float that represents the stock's price percentage increase on the next day 
+        :return target_fore: tunned 1 day forecast of the stock's price
+        :return perc: tunned stock's price percentage increase on the next day 
+        """
         target = stock_data['Close']
         if target[-1] > 5000:
             cap = 20
@@ -57,7 +78,14 @@ class TimeSeriesPredictions:
         return (target_fore, perc)
     
     
-    def create_plot(self, stock_data, target_pred, target_fore, ax, emiten):
+    def create_plot(self, stock_data, target_pred, target_fore, ax, emiten): 
+        """Creates the visualisation for the stock's price, stock price predictions, and stock price forecast
+        :param stock_data: a pandas dataframe that contain the emiten's stocks data
+        :param target_pred: a numpy arrays consisting of predictions made for the stock's price
+        :param target_fore: a numpy arrays that consist 1 day forecast of the stock's price
+        :param ax: an integere that repersentes the axes for which the plots will be visualized
+        :param emiten: a string that represents the emiten's code that is listed in the stock market
+        """
         stock_data['Close'].plot(ax=ax)
         target_pred.plot(ax=ax)
         target_fore.plot(ax=ax)
@@ -81,7 +109,3 @@ class TimeSeriesPredictions:
             return
         
         return (target_fore, perc, rmse)
-
-
-
-
